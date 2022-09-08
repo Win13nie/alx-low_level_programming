b5b74661c9baeb33c08a77ff526ceb7ffe426519
winnie wahome <wahomewinn00@gmail.com>
2022-09-08 06:24:36 -0700

    c4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,23 @@
-# include <stdio.h>
-int main() {
+#include <stdio.h>
 
-	   puts(""Programming is like building a multilingual puzzle");
-	   return 0;
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
