a64293030557dd2dfc0427651d5a66864b24205d
winnie wahome <wahomewinn00@gmail.com>
2022-09-08 11:13:15 -0700

    c6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -36,8 +36,8 @@ int main(void)
 	 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 
 	 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	 
-         return (0);
+
+	 return (0);
 
 }
 
