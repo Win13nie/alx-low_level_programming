8aac3060e7960360d0115b4b3a1677012872bfaa
winnie wahome <wahomewinn00@gmail.com>
2022-09-09 12:08:44 -0700

    c8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - prints the standrad error sandwiched around the standard outpu/t
  *
  * Return: returns 1 (success)
- *
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+		       	59);
 	return (1);
 
 }
