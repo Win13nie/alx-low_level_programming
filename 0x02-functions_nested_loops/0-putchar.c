9cd8079dfa2042c09cecf9bafed1fb28c0e132e6
winnie wahome <wahomewinn00@gmail.com>
2022-09-15 16:07:12 -0700

    t0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print _putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
