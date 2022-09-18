ae98d643edab6ca45f8eb0baf2d01e1b28ce559b
winnie wahome <wahomewinn00@gmail.com>
2022-09-18 07:50:15 -0700

    t4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase apart from q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == 'q'&&x == 'e')
+		{
+			continue;
+		}
+
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
