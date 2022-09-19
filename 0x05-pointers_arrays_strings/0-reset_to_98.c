5298e0c52398945cf928dce1f2597437d4561a3e
winnie wahome <wahomewinn00@gmail.com>
2022-09-19 14:59:49 -0700

    t1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,14 @@
 
 /**
  * main - a pointer that updates the value of a variable to 98
+ * @n - variable to be changes
  *
  * Return: 0
  */
+void reset_to_98(int *n)
+{
+	n = 402;
+}
 
 int main(void)
 {
@@ -19,6 +24,8 @@ int main(void)
 	printf("value of 'p' : %d\n", n);
 
 	*p = 98;
+
+	reset_to_98(int *n);
 
 	printf("value of 'n' : %d\n", n);
 
