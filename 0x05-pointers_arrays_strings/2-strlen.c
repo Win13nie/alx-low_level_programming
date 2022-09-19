ee5226ddefedb10e68bed512443bd7027dc2cd4c
winnie wahome <wahomewinn00@gmail.com>
2022-09-19 16:38:24 -0700

    t2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; ++s)
+	{
+		++counter;
+	}
+
+	return (counter);
+}
