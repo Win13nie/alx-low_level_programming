2e79422f6f00886da45357a6b9d140ce96e6e557
winnie wahome <wahomewinn00@gmail.com>
2022-09-19 17:08:46 -0700

    t4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; ++index)
+		;
+
+	for (--index; index >= 0; --index)
+	{
+		putchar(s[index]);
+	}
+
+	putchar('\n');
+}
