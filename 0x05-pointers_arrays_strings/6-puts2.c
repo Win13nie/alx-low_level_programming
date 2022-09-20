b62c7627587cdd991fcaf8000e4b27be5b099f47
winnie wahome <wahomewinn00@gmail.com>
2022-09-20 04:26:08 -0700

    t6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - this prints every character of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
