c5fc2af5af7afea601f8ef306bdeecae331c21d8
winnie wahome <wahomewinn00@gmail.com>
2022-09-26 21:24:09 -0700

    t2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer
+ * @c: character to be returned
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	/**
+	 * if c is '\0', you should return
+	 * the pointer to the '\0' of the
+	 * string s
+	*/
+	if (*s == c)
+		return (s);
+	/*return null if not found*/
+	return ('\0');
+}
