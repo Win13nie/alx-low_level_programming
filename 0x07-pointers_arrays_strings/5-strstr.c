aa140e5ed322851ee3c4b29ff57f25aea1f60c10
winnie wahome <wahomewinn00@gmail.com>
2022-09-26 22:22:47 -0700

    t5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#incude "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @needle: substring to locate
+ * @haystack: string tgat contains needle
+ *
+ * Return: pointer to the beginning of the located substring, or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+		haystack++;
+	}
+	return ('\0');
+}
