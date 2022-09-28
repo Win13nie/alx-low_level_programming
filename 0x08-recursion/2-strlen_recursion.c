748238ed786090eaa0871fbe52168cb8c4a12a41
winnie wahome <wahomewinn00@gmail.com>
2022-09-28 09:58:28 -0700

    t2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,13 @@
 
 /**
  * _strlen_recursion - function that returns the length of a string
- * @s: pointer
- * Return: 0
+ * @s: string
+ * Return: length
  */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 
 		return (1 + _strlen_recursion(s + 1));
 	else
