f67e6cf0eaa33966924b7a4ec22a74807546ce3c
winnie wahome <wahomewinn00@gmail.com>
2022-10-02 14:39:18 -0700

    t0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints out its name
+ * @argc: number of commands on command line
+ * @argv: arrays
+ * Return: 0
+ */
+
+int main( int argc, char *argv[])
+{
+	printf("%s\n", argv[argc *0]);
+
+	return (0);
+}
