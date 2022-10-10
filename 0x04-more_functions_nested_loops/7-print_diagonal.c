1a5a57ded04d352ef1390956c31b5a5084762a6d
winnie wahome <wahomewinn00@gmail.com>
2022-10-10 11:18:35 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	} else
 	{
 		int i, j;
@@ -18,11 +18,11 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-					_putchar('\\');
+					putchar('\\');
 				else if (j < i)
-					_putchar(' ');
+					putchar(' ');
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
