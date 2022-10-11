aabcbf69f5c29c674376702ca82dd1efe7a3cace
winnie wahome <wahomewinn00@gmail.com>
2022-10-11 08:29:38 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,10 @@
-#ifndef FUNCTION_LIKE_MACRO_H_
-#define FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-/**
- * file - function_like_macro.h
- * Desc:a header file that defines a function-like macro ABS(x) that computes the absolute value of a number x
- *
+/*
+ * File: 3-function_like_macro.h
+ * Desc: a Header file dat defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
