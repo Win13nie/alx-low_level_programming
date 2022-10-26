c59382d756ed546e6e27d9e96a9f865049752a19
winnie wahome <wahomewinn00@gmail.com>
2022-10-26 02:40:50 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t elements = 0;
+	size_t nn = 0;
 	
 	while(h != NULL)
 	{
+		printf("%d\n", h->n);
 		h = h->next;
-		elements++;
+		nn++;
 	}
 
-	return elements;
+	return nn;
 }
