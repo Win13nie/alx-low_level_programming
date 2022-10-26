306dafdc731818ceec745c3788cb90b80a0ef57b
winnie wahome <wahomewinn00@gmail.com>
2022-10-26 03:08:35 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints all tge elements
+ * @h: The listint_t list.
+ * Return: returns the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t element = 0;
+
+	while(h != NULL)
+	{
+		h = h->next;
+		element++;
+	}
+	return element;
+}
